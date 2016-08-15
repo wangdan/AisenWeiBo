274dbd91d6f6bb7589485dc65a5ecaf4d6da60d9
wangdan <wangd.info@gmail.com>
2016-08-15 19:35:15 +0800

    菜单栏新增ICON

--- app/src/main/java/org/aisen/weibo/sina/ui/fragment/menu/MenuFragment.java
+++ app/src/main/java/org/aisen/weibo/sina/ui/fragment/menu/MenuFragment.java
@@ -287,6 +287,7 @@ public class MenuFragment extends ABaseFragment {
 //        items.add(new NavMenuItem(MENU_CMT, R.drawable.ic_question_answer_grey600_24dp, R.string.draw_comment));
         items.add(new NavMenuItem(MENU_NOTIFICATION, R.drawable.ic_notification_gray_24, R.string.draw_private_notification));
         items.add(new NavMenuItem(MENU_MD, R.drawable.ic_email_grey600_24dp, R.string.draw_private_msg));
+        items.add(new NavMenuItem(MENU_FAV, R.drawable.ic_unfavorite_dark, R.string.draw_fav, R.string.myfav_title));
 
         items.add(new NavMenuSeparator());
 
@@ -298,9 +299,9 @@ public class MenuFragment extends ABaseFragment {
 //        items.add(new NavMenuItem(MENU_JOKE, -1, R.string.draw_joke));
 //        items.add(new NavMenuItem(MENU_WALLPAPER, -1, R.string.draw_wallpaper));
 //        items.add(new NavMenuItem(MENU_IMAGES, -1, R.string.draw_images));
-        items.add(new NavMenuItem(MENU_FAV, -1, R.string.draw_fav));
 
-        items.add(new NavMenuSeparator());
+
+//        items.add(new NavMenuSeparator());
 
         items.add(new NavMenuItem(MENU_DRAT, -1, R.string.draw_draft));
         items.add(new NavMenuItem(MENU_SETTINGS, -1, R.string.draw_settings));
